495272e6822372ad84a8c1816c98b092a5f6ce1a
Dan Do <dan.do@devbase.us>
2023-09-22 14:23:26 +0700

    update

--- dist/assets/keyboard2-legacy-eabfb97b.js
+++ dist/assets/keyboard2-legacy-eabfb97b.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-398faee7.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-945962de.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- dist/assets/md.transition-legacy-014ee751.js
+++ dist/assets/md.transition-legacy-014ee751.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-398faee7.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
+System.register(["./index-legacy-945962de.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
