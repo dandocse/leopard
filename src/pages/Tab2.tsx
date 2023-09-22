b5457acd5d19893871c1619838a24e0bd6d9c904
Dan Do <dan.do@devbase.us>
2023-09-22 11:34:15 +0700

    update

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCol, IonGrid, IonRow } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
@@ -11,9 +11,43 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        <IonHeader >
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+          <IonGrid>
+        <IonRow>
+          <IonCol sizeMd='10' sizeSm='1'>1</IonCol>
+          <IonCol>2</IonCol>
+          <IonCol>3</IonCol>
+        </IonRow>
+      </IonGrid>
+
+      <IonGrid>
+        <IonRow>
+          <IonCol>1</IonCol>
+          <IonCol>2</IonCol>
+          <IonCol>3</IonCol>
+          <IonCol>4</IonCol>
+          <IonCol>5</IonCol>
+          <IonCol>6</IonCol>
+        </IonRow>
+      </IonGrid>
+
+      <IonGrid>
+        <IonRow>
+          <IonCol>1</IonCol>
+          <IonCol>2</IonCol>
+          <IonCol>3</IonCol>
+          <IonCol>4</IonCol>
+          <IonCol>5</IonCol>
+          <IonCol>6</IonCol>
+          <IonCol>7</IonCol>
+          <IonCol>8</IonCol>
+          <IonCol>9</IonCol>
+          <IonCol>10</IonCol>
+          <IonCol>11</IonCol>
+          <IonCol>12</IonCol>
+        </IonRow>
+      </IonGrid>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 2 page" />
